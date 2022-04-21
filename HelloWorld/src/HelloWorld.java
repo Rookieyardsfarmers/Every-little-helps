d7471439c5cf7bb30cf0eacf4c576795615b53f2
Rookieyardsfarmers <1774550198@qq.com>
2022-04-21 23:47:02 +0800

    GitHub的第一次提交  来自程序员的入门第一课 Hello World

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author: yangzhenpeng
+ * @date: 2022/4/21 23:42
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
