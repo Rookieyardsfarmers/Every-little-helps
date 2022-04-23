8a2665e128e22ffb574a3051fc7897d33b9a7fcc
Rookieyardsfarmers <1774550198@qq.com>
2022-04-23 21:51:00 +0800

    学习C++语法

---
+++ learncpp/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+struct free_throws{
+    string name;
+    int made;
+    int attempts;
+    float percent;
+};
+void display(const free_throws & ft);
+void set_pc(free_throws & ft);
+free_throws & accumulate(free_throws &  target, const free_throws & source);
+int main() {
+    free_throws one = {"one",13,14};
+    free_throws two = {"two",10,16};
+    free_throws three = {"three",0,0};
+
+    free_throws dup;
+    set_pc(one);
+    display(one);
+    accumulate(three,one);
+    display(three);
+    display(accumulate(three,two));
+    accumulate(accumulate(three,one),two);
+    display(three);
+
+    dup = accumulate(three,one);
+    cout<<"displaying dup:\n";
+    display(dup);
+
+    return 0;
+}
+void display(const free_throws & ft){
+    cout<<"Name: "<<ft.name<<"\n";
+    cout<<"Made: "<<ft.made<<"\t";
+    cout<<"Attempts: "<<ft.attempts<<"\t";
+    cout<<"Percent: "<<ft.percent<<"\n";
+}
+void set_pc(free_throws & ft){
+    if (ft.attempts!=0)
+        ft.percent = 100.0f *float (ft.made)/float (ft.attempts);
+    else
+        ft.percent = 0;
+}
+
+free_throws & accumulate(free_throws & target, const free_throws & source){
+    target.attempts  += source.attempts;
+    target.made += source.made;
+    set_pc(target);
+    return target;
+}
+
+
+
+
+
+
+
+
